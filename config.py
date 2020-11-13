f1842901f2f7aa3fbacf4a8fa420ca0d1c9f4708
iMeleon <scentofdarkness@gmail.com>
2020-11-13 19:27:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 DATA_DIR = "/content/gdrive/My Drive/darknet/bin/darknet/stack_rcnn_2_new"
+SAVE_PATH = "/content/gdrive/My Drive/darknet/bin/darknet/weights/state_dict_model.pt"
 BATCH_SIZE = 1
-IMAGE_WIDTH = 300
-IMAGE_HEIGHT = 75
+IMAGE_WIDTH = 150
+IMAGE_HEIGHT = 38
 NUM_WORKERS = 8
 EPOCHS = 200
 DEVICE = "cuda"
