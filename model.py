47a34c5e3102aecfb667014256dfca60b6fbe6d2
iMeleon <scentofdarkness@gmail.com>
2020-11-13 20:41:10 +0300

    derease hidden layers

--- model.py
+++ model.py
@@ -10,7 +10,7 @@ class CaptchaModel(nn.Module):
         self.pool_1 = nn.MaxPool2d(kernel_size=(2, 2))
         self.conv_2 = nn.Conv2d(128, 64, kernel_size=(3, 6), padding=(1, 1))
         self.pool_2 = nn.MaxPool2d(kernel_size=(2, 2))
-        self.linear_1 = nn.Linear(1152, 64)
+        self.linear_1 = nn.Linear(576, 64)
         self.drop_1 = nn.Dropout(0.2)
         self.lstm = nn.GRU(64, 32, bidirectional=True, num_layers=2, dropout=0.25, batch_first=True)
         self.output = nn.Linear(64, num_chars + 1)
