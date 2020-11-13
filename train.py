14abf2de2a4324f2568cdf6d73cf938542a10ef8
iMeleon <scentofdarkness@gmail.com>
2020-11-13 23:25:25 +0300

    Update train.py

--- train.py
+++ train.py
@@ -54,7 +54,7 @@ def decode_predictions(preds, encoder):
 
 def run_training():
     image_files = glob.glob(os.path.join(config.DATA_DIR, "*.png"))
-    targets_orig = [x.split("/")[-1][:-4] for x in image_files]
+    targets_orig = [x.split("/")[-1][:-4].split('_')[0] for x in image_files]
     targets = [[c for c in x] for x in targets_orig]
     targets_flat = [c for clist in targets for c in clist]
 
