eb18e64db4191042dd98d31136e7703c17821e7d
Artur <scentofdarkness@gmail.com>
2020-11-14 14:28:14 +0300

    edit cnfig

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 DATA_DIR = "/content/gdrive/My Drive/darknet/bin/darknet/stack_rcnn_2_new"
-MODEL_SAVE_PATH = "/content/gdrive/My Drive/darknet/bin/darknet/weights/state_dict_model.pt"
+MODEL_SAVE_PATH = "/content/gdrive/My Drive/darknet/bin/darknet/weights/model.pth"
 BATCH_SIZE = 1
 IMAGE_WIDTH = 150
 IMAGE_HEIGHT = 38
